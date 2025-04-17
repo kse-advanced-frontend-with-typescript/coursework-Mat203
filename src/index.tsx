900c599062bfbf0b53333454cf56966699125e5f
Mat203 <matvey.talalevskiy@gmail.com>
2025-04-17 10:10:05 +0300

    debug

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
-import { AppProvider } from './context';
 import { CartProvider } from '../modules/cart/CartContext';
 
 const root = ReactDOM.createRoot(
@@ -12,11 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <AppProvider>
                 <CartProvider>
                     <App />
                 </CartProvider>
-            </AppProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
